a14dd7de9a0b162f0c50fca5b353ce1f5d265850
Diego Tsuyoshi <dts2896@gmail.com>
2023-01-29 13:50:03 -0400

    stage 2 clear

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -12,7 +12,25 @@ fn main() {
       Ok(mut stream) => {
         println!("accepted new connection");
 
-        stream.write("+PONG\r\n".as_bytes()).unwrap();
+        // first request
+        let mut buffer = [0; 512];
+
+        let mut is_connection_open = true;
+
+        while is_connection_open {
+          let bytes_read = stream.read(&mut buffer).unwrap();
+
+          if bytes_read == 0 {
+            println!("Client closed the connection");
+            is_connection_open = false;
+          }
+
+          let received_string = String::from_utf8(buffer.to_vec()).unwrap();
+
+          println!("Text gotten after first opening connection: {received_string}");
+
+          stream.write("+PONG\r\n".as_bytes()).unwrap();
+        }
       }
       Err(e) => {
         println!("error: {}", e);
